5d8f0f7de9be5a19183270396f8150aae02f8ed8
akozlowski <adrian.kozlowski@outlook.com>
2016-10-07 02:04:39 +0200

    Change nick

--- src/pl/quider/standalone/irc/Main.java
+++ src/pl/quider/standalone/irc/Main.java
@@ -1,7 +1,11 @@
 package pl.quider.standalone.irc;
 
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.NickAlreadyInUseException;
 import pl.quider.standalone.irc.dbsession.ADatabaseSession;
 import pl.quider.standalone.irc.dbsession.MySqlDatabaseSession;
+
+import java.io.IOException;
 
 /**
  * Created by Adrian on 27.09.2016.
@@ -20,8 +24,15 @@ public class Main {
         bot.setVerbose(true);
 
         // Connect to the IRC server.
-        bot.connect("open.ircnet.net");
+        bot.changeNick("Adirael");
+        try {
+            bot.connect("open.ircnet.net");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NickAlreadyInUseException e) {
+            bot.changeNick("_Adirael");
 
+        }
         // Join the #pircbot channel.
         bot.joinChannel("#pircbot");
 
