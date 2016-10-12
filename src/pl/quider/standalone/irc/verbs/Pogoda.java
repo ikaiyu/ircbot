75f15458d861aae7d6a06ed9a6b0df1b8977ce85
akozlowski <adrian.kozlowski@outlook.com>
2016-10-13 00:10:32 +0200

    Pogoda refactorisation

--- src/pl/quider/standalone/irc/verbs/Pogoda.java
+++ src/pl/quider/standalone/irc/verbs/Pogoda.java
@@ -18,6 +18,12 @@ import java.net.URLConnection;
  * Created by Adrian on 12.10.2016.
  */
 public class Pogoda extends Verb {
+    private static final String TEMPERATURE = "temperature";
+    private static final String VALUE = "value";
+    private static final String PRESSURE = "pressure";
+    private static final String HUMIDITY = "humidity";
+    public static final String POST = "POST";
+
     private static String API_CODE = "&APPID=4c1606b8ad9cc95f39e300113634e47f&mode=xml";
     private static String URL = "http://api.openweathermap.org/data/2.5/forecast/city?q=";
     private URL url;
@@ -32,49 +38,73 @@ public class Pogoda extends Verb {
         String[] split = parameter.split(MyBot.VERB_PARAM_DELIMITER);
         if (split.length == 1) {
             try {
-                this.url = new URL(URL + split[0] + API_CODE);
-                HttpURLConnection request = (HttpURLConnection) url.openConnection();
-                request.setRequestMethod("POST");
-                InputStream is = request.getInputStream();
-                BufferedInputStream bis = new BufferedInputStream(is);
-                BufferedReader br = new BufferedReader(new InputStreamReader(bis));
-                String inputLine = "";
-                StringBuffer sb = new StringBuffer();
+                InputStream is = getInputStream(split);
 
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                Document document = builder.parse(is);
-                Element root = document.getDocumentElement();
-
-                NodeList forecast = root.getElementsByTagName("forecast");
-                Node firstChild = forecast.item(0).getFirstChild();
-                NodeList childNodes = firstChild.getChildNodes();
-                StringBuilder sbMessage= new StringBuilder();
-                for (int i = 0; i <= childNodes.getLength() - 1; i++) {
-                    Node item = childNodes.item(i);
-                    if (item.getNodeName().equals("temperature")) {
-                        Node value = item.getAttributes().getNamedItem("value");
-                        sbMessage.append("Temp: ").append(value.getNodeValue()).append("*C, ");
-                    } else if (item.getNodeName().equals("pressure")) {
-                        Node value = item.getAttributes().getNamedItem("value");
-                        sbMessage.append("Ciśnienie: ").append(value.getNodeValue()).append("hPa, ");
-                    } else if (item.getNodeName().equals("humidity")) {
-                        Node value = item.getAttributes().getNamedItem("value");
-                        sbMessage.append("Wilgotność: ").append(value.getNodeValue()).append("%.");
-                    }
-                }
+                NodeList childNodes = getNodeList(is);
+                StringBuilder sbMessage = getStringBuilder(childNodes);
 
                 String s = sbMessage.toString();
-                bot.sendMessage(msg.getChannel(),s);
+                this.sendMessage(s);
+                sendMessage(s);
             } catch (IOException e) {
-                bot.sendMessage(msg.getChannel(),"Em... Coś nie halo...");
+                sendMessage("Em... Coś nie halo...");
+                e.printStackTrace();
             } catch (ParserConfigurationException e) {
+                sendMessage("Konfiguracja nie bangla");
                 e.printStackTrace();
             } catch (SAXException e) {
+                sendMessage("Xml z webserwisu jest do bani");
                 e.printStackTrace();
             } catch (DOMException e) {
+                sendMessage("Rozjechało się parsowanie");
                 e.printStackTrace();
             }
         }
     }
+
+    private InputStream getInputStream(String[] split) throws IOException {
+        this.url = new URL(URL + split[0] + API_CODE);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.setRequestMethod(POST);
+        return request.getInputStream();
+    }
+
+    private StringBuilder getStringBuilder(NodeList childNodes) {
+        StringBuilder sbMessage= new StringBuilder();
+
+        for (int i = 0; i <= childNodes.getLength() - 1; i++) {
+            Node item = childNodes.item(i);
+            if (item.getNodeName().equals(TEMPERATURE)) {
+                Node value = item.getAttributes().getNamedItem(VALUE);
+                sbMessage.append("Temp: ").append(value.getNodeValue()).append("*C, ");
+            } else if (item.getNodeName().equals(PRESSURE)) {
+                Node value = item.getAttributes().getNamedItem(VALUE);
+                sbMessage.append("Ciśnienie: ").append(value.getNodeValue()).append("hPa, ");
+            } else if (item.getNodeName().equals(HUMIDITY)) {
+                Node value = item.getAttributes().getNamedItem(VALUE);
+                sbMessage.append("Wilgotność: ").append(value.getNodeValue()).append("%.");
+            }
+        }
+        return sbMessage;
+    }
+
+    private NodeList getNodeList(InputStream is) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(is);
+        Element root = document.getDocumentElement();
+        NodeList forecast = root.getElementsByTagName("forecast");
+        Node firstChild = forecast.item(0).getFirstChild();
+        return firstChild.getChildNodes();
+    }
+
+    /**
+     *
+     * @param message
+     */
+    private void sendMessage(String message) {
+        bot.sendMessage(msg.getChannel(), message);
+    }
+
+
 }
