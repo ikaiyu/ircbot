d730e5f290d51c61c4e6b7b6eace42d01bf52cf5
akozlowski <adrian.kozlowski@outlook.com>
2016-10-14 00:14:40 +0200

    Kiedy verb

---
+++ src/pl/quider/standalone/irc/verbs/Kiedy.java
@@ -0,0 +1,18 @@
+package pl.quider.standalone.irc.verbs;
+
+import pl.quider.standalone.irc.MyBot;
+import pl.quider.standalone.irc.model.Message;
+
+/**
+ * Created by Adrian on 14.10.2016.
+ */
+public class Kiedy extends Verb {
+    public Kiedy(MyBot mybot, Message msg) {
+        super(mybot, msg);
+    }
+
+    @Override
+    public void execute(String parameter) throws Exception {
+
+    }
+}
---
+++ test/pl/quider/standalone/irc/verbs/KiedyTest.java
@@ -0,0 +1,16 @@
+package pl.quider.standalone.irc.verbs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Adrian on 14.10.2016.
+ */
+public class KiedyTest {
+    @Test
+    public void execute() throws Exception {
+
+    }
+
+}
