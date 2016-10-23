420acfd1f11d05c643745a49c417386459256dde
akozlowski <adrian.kozlowski@outlook.com>
2016-10-23 12:40:09 +0200

    Kiedy verb fixed

--- src/pl/quider/standalone/irc/ConfigurationKeysContants.java
+++ src/pl/quider/standalone/irc/ConfigurationKeysContants.java
@@ -3,7 +3,7 @@ package pl.quider.standalone.irc;
 /**
  * Created by Adrian on 20.10.2016.
  */
-public class ConfigurationKeysContants {
+public interface ConfigurationKeysContants {
     public static final String NICK = "nick";
     public static final String SERVER1 = "server1";
     public static final String ALT_NICK = "alt_nick";
--- src/pl/quider/standalone/irc/verbs/Kiedy.java
+++ src/pl/quider/standalone/irc/verbs/Kiedy.java
@@ -1,5 +1,7 @@
 package pl.quider.standalone.irc.verbs;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.procedure.ParameterMisuseException;
 import pl.quider.standalone.irc.MyBot;
 import pl.quider.standalone.irc.model.Message;
@@ -11,24 +13,33 @@ import java.util.Date;
  * Created by Adrian on 14.10.2016.
  */
 public class Kiedy extends Verb {
+    private static final Logger LOG = LogManager.getLogger("Kiedy");
+
     public Kiedy(MyBot mybot, Message msg) {
         super(mybot, msg);
     }
 
     @Override
     public void execute(String parameter) throws Exception {
+        LOG.info("Kiedy verb fired");
         final String[] split = parameter.split(MyBot.VERB_PARAM_DELIMITER);
-        if(split.length >1){
+        if(split.length >1 || split.length == 0){
             throw new ParameterMisuseException("Tylko jeden nick na raz");
         }
-
+        LOG.debug("parameter: "+ split[0].toString());
         final UserService userService = new UserService(split[0], null, null, bot.getSession());
-        final Date seen = userService.seen(split[0]);
+        Date seen = null;
+        seen = userService.seen(split[0]);
         this.sendMessage(seen, split[0]);
     }
 
     private void sendMessage(Date seen, String nickname) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Ostatni raz ").append(nickname).append(" pisał(a) ").append(seen);
+        if(seen != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Ostatni raz ").append(nickname).append(" pisał(a) ").append(seen);
+            bot.sendMessage(msg.getChannel(), sb.toString());
+        } else {
+            bot.sendMessage(msg.getChannel(), "Nic nie mówi mi nick "+nickname);
+        }
     }
 }
