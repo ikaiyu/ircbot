ee13a25bc04333de0777565e4a3e54940b95d7dc
Adrian <quider@users.noreply.github.com>
2016-10-23 21:01:23 +0200

    Rss fixes for atom feeds (#2)

--- src/pl/quider/standalone/irc/services/RssService.java
+++ src/pl/quider/standalone/irc/services/RssService.java
@@ -24,10 +24,8 @@ public class RssService {
     public static final String SELECT_RSS_BY_URI = "select r from pl.quider.standalone.irc.model.RssEntry as r where r.uri = :link";
 
     public List<pl.quider.standalone.irc.model.Rss> getListOfFeeds() {
-
         Query query = session.createQuery(SELECT_ALL_RSS);
         List resultList = query.getResultList();
-
         return resultList;
     }
 
@@ -43,16 +41,28 @@ public class RssService {
         query.setParameter("link", uri);
         List<RssEntry> resultList = query.getResultList();
         if (resultList.size() == 0) {
-            RssEntry rss = new RssEntry();
-            rss.setTitle(title);
-            rss.setUri(uri);
-            rss.setAdded(new Date());
-            rss.setRss(rssParent);
-            session.save(rss);
+            RssEntry rssEntry = createRssEntry(title, uri, rssParent);
+            session.save(rssEntry);
             transaction.commit();
             return true;
         }
         transaction.rollback();
         return  false;
+    }
+
+    /**
+     * Just creates object of RssEntry and returns it
+     * @param title title of entry
+     * @param uri url of entry
+     * @param rssParent rss feed
+     * @return entry object
+     */
+    protected RssEntry createRssEntry(String title, String uri, Rss rssParent) {
+        RssEntry rss = new RssEntry();
+        rss.setTitle(title);
+        rss.setUri(uri);
+        rss.setAdded(new Date());
+        rss.setRss(rssParent);
+        return rss;
     }
 }
